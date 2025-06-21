1030baac66bf0fb5dbd95a4b6aefd0aa05e53fd5
Shobhit0398 <52675059+Shobhit0398@users.noreply.github.com>
2025-06-21 10:19:11 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "devops-recipe-app-shobhi"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
     encrypt        = true
   }
 }
